d8650cd71eb5b4363ed454c8d0c1e66af6653d47
Victoria <vitagood.92@list.ru>
2025-10-31 20:44:41 +0300

    finished 2.9

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import TripFiltersView from './view/trip-filters-view.js';
 import TripSortView from './view/trip-sort-view.js';
 import { render } from './render.js';
 import PointsPresenter from './presenter/points-presenter.js';
+import PointsModel from './model/point-model.js';
 
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteSortElement = document.querySelector('.trip-events');
 const pointsPresenter = new PointsPresenter();
 
+const pointsModel = new PointsModel();
+
 render(new TripFiltersView(), siteFilterElement);
 render(new TripSortView(), siteSortElement);
 
-pointsPresenter.init(siteSortElement);
+pointsPresenter.init(siteSortElement, pointsModel);
 
 
